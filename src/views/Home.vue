e21f79288e199e75c8a22c368450ef7176b9d350
Taka <Yuhdaibrahim8@gmail.com>
2020-02-29 11:26:08 +0700

    done slicing html to vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="home">
+  <headerWeb />
+  <carouselComponent />
+  <carouselWomenComp />
+  <galleryInstaComp />
+  <logoComp />
+  <footerComp />
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import headerWeb from '@/components/header.vue';
+import carouselComponent from '@/components/carouselComponent.vue';
+import carouselWomenComp from '@/components/carouselWomenComp.vue';
+import galleryInstaComp from '@/components/galleryInstaComp.vue';
+import logoComp from '@/components/logoComp.vue';
+import footerComp from '@/components/footerComp.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    headerWeb,
+    carouselComponent,
+    carouselWomenComp,
+    galleryInstaComp,
+    logoComp,
+    footerComp
+  }
+};
+</script>
